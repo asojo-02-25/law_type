232e5e50094ad1ad06799e6c6fae348b962f4b83
Jotaro Aso <joutarouasou@gmail.com>
2025-12-01 16:47:47 +0000

    ゲームスタート時の画面の作成

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-//設定画面→ゲーム画面の切り替え
+//設定画面→ゲーム画面の切り替え/ゲーム開始の処理
+
+const form = document.querySelector('#form')
+form.addEventListener('submit', (event) =>
+    event.preventDefault()
+);
 
 const btn = document.querySelector('#start-button')
 const startScreen = document.querySelector('#start-screen')
 const gameScreen = document.querySelector('#game-screen')
 const resultsScreen = document.querySelector('#results-screen')
 
-btn.addEventListener('click', startGame);
-function startGame(){
+const startGame = () => {
     //startScreenの非表示
     startScreen.style.display = 'none';
     //gameScreenの表示
     gameScreen.style.display = 'block';
 }
+
+btn.addEventListener('click', startGame);
+
