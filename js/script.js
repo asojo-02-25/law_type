92cfe67fdfffb4cf37d07f5324c7dbbd41c24b28
Jotaro Aso <joutarouasou@gmail.com>
2025-12-01 07:48:18 +0000

    css諸々変更

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
-// スタートボタンにマウスが重なったときにボタンを大きくする
-const btn = document.querySelector('#start-button')
-
-btn.addEventListener('mouseover', () => {
-    btn.animate(
-        {
-            
-        },
-        {
-            duration: .2
-        }
-    );
-});
-
 //設定画面→ゲーム画面の切り替え
 
+const btn = document.querySelector('#start-button')
+const startScreen = document.querySelector('#start-screen')
+const gameScreen = document.querySelector('#game-screen')
+const resultsScreen = document.querySelector('#results-screen')
 
+btn.addEventListener('click', startGame);
+function startGame(){
+    //startScreenの非表示
+    startScreen.style.display = 'none';
+    //gameScreenの表示
+    gameScreen.style.display = 'block';
+}
