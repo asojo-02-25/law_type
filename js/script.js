c06a5fe7e15aeeebe267465db9196b9a57c550b2
Jotaro Aso <joutarouasou@gmail.com>
2025-12-04 07:54:02 +0000

    ゲームエリアの遅延出現途中

--- js/script.js
+++ js/script.js
@@ -9,12 +9,24 @@ const btn = document.querySelector('#start-button')
 const startScreen = document.querySelector('#start-screen')
 const gameScreen = document.querySelector('#game-screen')
 const resultsScreen = document.querySelector('#results-screen')
+const displayDelay = document.querySelector('#display-delay')
 
 const startGame = () => {
     //startScreenの非表示
     startScreen.style.display = 'none';
     //gameScreenの表示
     gameScreen.style.display = 'block';
+    //問題欄、回答欄の遅延出現
+    const keyframes = {
+        opacity: [0, 1],
+    };
+    const options = {
+        duration: 1000,
+        easing: 'ease',
+        fill: 'forwards',
+    }
+
+    displayDelay.animate(keyframes, options);
 }
 
 btn.addEventListener('click', startGame);
